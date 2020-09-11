c39cad9b84aa8ba522aea8a3f1ae5d8909f6c347
stefanprodan <stefan.prodan@gmail.com>
2020-09-11 15:47:46 +0300

    Add watch all namespaces flag

--- main.go
+++ main.go
@@ -55,21 +55,29 @@ func main() {
 		concurrent           int
 		logLevel             string
 		logJSON              bool
+		watchAllNamespaces   bool
 	)
+
 	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&eventsAddr, "events-addr", ":9090", "The address the event endpoint binds to.")
 	flag.StringVar(&receiverAddr, "receiverAddr", ":9292", "The address the webhook receiver endpoint binds to.")
-
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
 	flag.IntVar(&concurrent, "concurrent", 4, "The number of concurrent notification reconciles.")
 	flag.StringVar(&logLevel, "log-level", "info", "Set logging level. Can be debug, info or error.")
 	flag.BoolVar(&logJSON, "log-json", false, "Set logging to JSON format.")
+	flag.BoolVar(&watchAllNamespaces, "watch-all-namespaces", true,
+		"Watch for custom resources in all namespaces, if set to false it will only watch the runtime namespace.")
 	flag.Parse()
 
 	zapLogger := logger.NewLogger(logLevel, logJSON)
 	ctrl.SetLogger(zapLogger)
+
+	watchNamespace := ""
+	if !watchAllNamespaces {
+		watchNamespace = os.Getenv("RUNTIME_NAMESPACE")
+	}
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:             scheme,
@@ -77,7 +85,7 @@ func main() {
 		Port:               9443,
 		LeaderElection:     enableLeaderElection,
 		LeaderElectionID:   "4ae6d3b3.fluxcd.io",
-		Namespace:          os.Getenv("RUNTIME_NAMESPACE"),
+		Namespace:          watchNamespace,
 		Logger:             ctrl.Log,
 	})
 	if err != nil {
