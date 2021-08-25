70363110cc13937a04cde5eb5ff57899a3476928
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2021-08-25 22:57:47 +0100

    Use Lark message cards for notification
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>

--- internal/notifier/lark.go
+++ internal/notifier/lark.go
@@ -1,18 +1,51 @@
 package notifier
 
-import "github.com/fluxcd/pkg/runtime/events"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/fluxcd/pkg/runtime/events"
+)
 
 type Lark struct {
 	URL string
 }
 
 type LarkPayload struct {
-	MsgType string      `json:"msg_type"`
-	Content LarkContent `json:"content"`
+	MsgType string   `json:"msg_type"`
+	Card    LarkCard `json:"card"`
 }
 
-type LarkContent struct {
-	Text string `json:"text"`
+type LarkCard struct {
+	Config LarkConfig `json:"config"`
+
+	Header LarkHeader `json:"header"`
+
+	Elements []LarkElement `json:"elements"`
+}
+
+type LarkConfig struct {
+	WideScreenMode bool `json:"wide_screen_mode"`
+}
+
+type LarkHeader struct {
+	Title    LarkTitle `json:"title"`
+	Template string    `json:"template"`
+}
+
+type LarkTitle struct {
+	Tag     string `json:"tag"`
+	Content string `json:"content"`
+}
+
+type LarkElement struct {
+	Tag  string   `json:"tag"`
+	Text LarkText `json:"text"`
+}
+
+type LarkText struct {
+	Tag     string `json:"tag"`
+	Content string `json:"content"`
 }
 
 func NewLark(address string) *Lark {
@@ -27,11 +60,46 @@ func (l *Lark) Post(event events.Event) error {
 		return nil
 	}
 
-	payload := LarkPayload{
-		MsgType: "text",
-		Content: LarkContent{
-			Text: event.Message,
+	emoji := "💫"
+	color := "turquoise"
+	if event.Severity == events.EventSeverityError {
+		emoji = "🚨"
+		color = "red"
+	}
+
+	message := fmt.Sprintf("**%s**\n\n", event.Message)
+	for k, v := range event.Metadata {
+		message = message + fmt.Sprintf("%s: %s\n", k, v)
+	}
+
+	element := LarkElement{
+		Tag: "div",
+		Text: LarkText{
+			Tag:     "lark_md",
+			Content: message,
 		},
+	}
+
+	card := LarkCard{
+		Config: LarkConfig{
+			WideScreenMode: true,
+		},
+		Header: LarkHeader{
+			Title: LarkTitle{
+				Tag: "plain_text",
+				Content: fmt.Sprintf("%s %s/%s.%s", emoji, strings.ToLower(event.InvolvedObject.Kind),
+					event.InvolvedObject.Name, event.InvolvedObject.Namespace),
+			},
+			Template: color,
+		},
+		Elements: []LarkElement{
+			element,
+		},
+	}
+
+	payload := LarkPayload{
+		MsgType: "interactive",
+		Card:    card,
 	}
 
 	return postMessage(l.URL, "", nil, payload)
