31da2939ff6d525454065c72f45a117b56f28104
Christopher Lillthors <christopher.lillthors@yubico.com>
2022-11-29 09:06:43 +0100

    Add tests for new Github app provider
    Signed-off-by: Christopher Lillthors <christopher.lillthors@yubico.com>

--- internal/notifier/github_dispatch_fuzz_test.go
+++ internal/notifier/github_dispatch_fuzz_test.go
@@ -18,7 +18,10 @@ package notifier
 
 import (
 	"context"
+	"crypto/rand"
+	"crypto/rsa"
 	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"io"
 	"net/http"
@@ -47,6 +50,54 @@ func Fuzz_GitHub_Dispatch(f *testing.F) {
 		_ = fuzz.NewConsumer(seed).GenerateStruct(&cert)
 
 		dispatch, err := NewGitHubDispatch(fmt.Sprintf("%s/%s", ts.URL, urlSuffix), token, &cert)
+		if err != nil {
+			return
+		}
+
+		event := eventv1.Event{}
+		// Try to fuzz the event object, but if it fails (not enough seed),
+		// ignore it, as other inputs are also being used in this test.
+		_ = fuzz.NewConsumer(seed).GenerateStruct(&event)
+
+		if event.Metadata == nil {
+			event.Metadata = map[string]string{}
+		}
+
+		event.Metadata["commit_status"] = commitStatus
+
+		_ = dispatch.Post(context.TODO(), event)
+	})
+}
+
+func Fuzz_GitHub_App_Dispatch(f *testing.F) {
+	f.Add(int64(1337), int64(1234), "org/repo", "", []byte{}, []byte{})
+	f.Add(int64(1337), int64(1234), "org/repo", "update", []byte{}, []byte{})
+	// f.Add(int64(0), int64(0), "", "", []byte{}, []byte{})
+
+	f.Fuzz(func(t *testing.T,
+		appID int64, installationID int64, urlSuffix, commitStatus string, seed, response []byte) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Write(response)
+			io.Copy(io.Discard, r.Body)
+			r.Body.Close()
+		}))
+		defer ts.Close()
+
+		key, err := rsa.GenerateKey(rand.Reader, 2048)
+		if err != nil {
+			return
+		}
+
+		var cert x509.CertPool
+		_ = fuzz.NewConsumer(seed).GenerateStruct(&cert)
+
+		addr := fmt.Sprintf("%s/%s", ts.URL, urlSuffix)
+		privateKey := pem.EncodeToMemory(
+			&pem.Block{
+				Type:  "PRIVATE KEY",
+				Bytes: x509.MarshalPKCS1PrivateKey(key),
+			})
+		dispatch, err := NewGitHubAppDispatch(addr, appID, installationID, privateKey, &cert)
 		if err != nil {
 			return
 		}
--- internal/notifier/github_dispatch_test.go
+++ internal/notifier/github_dispatch_test.go
@@ -18,14 +18,38 @@ package notifier
 
 import (
 	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+func generateTestRSAKey(t *testing.T) []byte {
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		t.Fatalf("rsa.GenerateKey() error = %v", err)
+	}
+	return pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "PRIVATE KEY",
+			Bytes: x509.MarshalPKCS1PrivateKey(key),
+		})
+}
+
 func TestNewGitHubDispatchBasic(t *testing.T) {
 	g, err := NewGitHubDispatch("https://github.com/foo/bar", "foobar", nil)
+	assert.Nil(t, err)
+	assert.Equal(t, g.Owner, "foo")
+	assert.Equal(t, g.Repo, "bar")
+	assert.Equal(t, g.Client.BaseURL.Host, "api.github.com")
+}
+
+func TestNewGitHubAppDispatchBasic(t *testing.T) {
+	g, err := NewGitHubAppDispatch("https://github.com/foo/bar", 1, 1, generateTestRSAKey(t), nil)
 	assert.Nil(t, err)
 	assert.Equal(t, g.Owner, "foo")
 	assert.Equal(t, g.Repo, "bar")
@@ -40,8 +64,21 @@ func TestNewEnterpriseGitHubDispatchBasic(t *testing.T) {
 	assert.Equal(t, g.Client.BaseURL.Host, "foobar.com")
 }
 
+func TestNewEnterpriseGitHubAppDispatchBasic(t *testing.T) {
+	g, err := NewGitHubAppDispatch("https://foobar.com/foo/bar", 1, 1, generateTestRSAKey(t), nil)
+	assert.Nil(t, err)
+	assert.Equal(t, g.Owner, "foo")
+	assert.Equal(t, g.Repo, "bar")
+	assert.Equal(t, g.Client.BaseURL.Host, "foobar.com")
+}
+
 func TestNewGitHubDispatchInvalidUrl(t *testing.T) {
 	_, err := NewGitHubDispatch("https://github.com/foo/bar/baz", "foobar", nil)
+	assert.NotNil(t, err)
+}
+
+func TestNewGitHubAppDispatchInvalidUrl(t *testing.T) {
+	_, err := NewGitHubAppDispatch("https://github.com/foo/bar/baz", 1, 1, generateTestRSAKey(t), nil)
 	assert.NotNil(t, err)
 }
 
@@ -50,8 +87,23 @@ func TestNewGitHubDispatchEmptyToken(t *testing.T) {
 	assert.NotNil(t, err)
 }
 
+func TestNewGitHubAppDispatchEmptyKey(t *testing.T) {
+	_, err := NewGitHubAppDispatch("https://github.com/foo/bar", 1, 1, []byte{}, nil)
+	assert.NotNil(t, err)
+}
+
 func TestGitHubDispatch_PostUpdate(t *testing.T) {
 	githubDispatch, err := NewGitHubDispatch("https://github.com/foo/bar", "foobar", nil)
+	require.NoError(t, err)
+
+	event := testEvent()
+	event.Metadata["commit_status"] = "update"
+	err = githubDispatch.Post(context.TODO(), event)
+	require.NoError(t, err)
+}
+
+func TestGitHubAppDispatch_PostUpdate(t *testing.T) {
+	githubDispatch, err := NewGitHubAppDispatch("https://github.com/foo/bar", 1, 1, generateTestRSAKey(t), nil)
 	require.NoError(t, err)
 
 	event := testEvent()
